0848238e9a864f8208abea545dd6c2b8ce96b644
Replit user <>
2023-08-24 20:39:05 +0000

    new try

--- list.c
+++ list.c
@@ -29,7 +29,7 @@ Node *createNode(void *data) {
 }
 
 List *createList() {
-  List *list = (List *)malloc(sizeof(list));
+  List *list = (List *)malloc(sizeof(List));
   list->head = NULL;
   list->tail = 0;
   list->current = NULL;
